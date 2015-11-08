84e043aca17864fd5bc2c92b63796170bbcb6fde
inguscrowe <inguscrowe@hotmail.com>
2015-11-08 10:27:16 -0600

    Update Manager.cs

--- ClassLibrary1/Manager.cs
+++ ClassLibrary1/Manager.cs
@@ -8,58 +8,6 @@ namespace ClassLibrary1
 {
     public class Manager
     {
-        public List<string> RunApp(int numberOfEntries)
-        {
-            List<string> allLines =  new List<string>();
-            for (int i = 1; i <= numberOfEntries; i++)
-            {
-                string allNumbers = "";
-                if (i % 3 == 0)
-                {
-                    allNumbers += "Fizz";
-                }
-                if (i % 5 == 0)
-                {
-                    allNumbers += "Buzz";
-                }
-                if (i % 3 != 0 && i % 5 != 0)
-                {
-                    allNumbers = i.ToString();
-                }
-                allLines.Add(allNumbers);
-            }
-            return allLines;
-
-        }
-
-        public List<string> RunApp(int lowEndNumber, int lastNumber)
-        {
-            List<string> allLines = new List<string>();
-            for (int i = lowEndNumber; i <= lowEndNumber+500; i++)
-            {
-                if(i>lastNumber)
-                {
-                    break;
-                }
-                string allNumbers = "";
-                if (i % 3 == 0)
-                {
-                    allNumbers += "Fizz";
-                }
-                if (i % 5 == 0)
-                {
-                    allNumbers += "Buzz";
-                }
-                if (i % 3 != 0 && i % 5 != 0)
-                {
-                    allNumbers = i.ToString();
-                }
-                allLines.Add(allNumbers);
-            }
-            return allLines;
-
-        }
-
         public List<string> FizzBuzzMethod(int lowEndNumber, int totalNumber, List<NumberWordPairing> pairings)
         {
             List<string> allLines = new List<string>();
